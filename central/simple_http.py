d6883262df9e9dc4cf035b38acfa021a2d110ceb
dRacz3 <daniel.racz@irob.uni-obuda.hu>
2021-02-27 11:48:10 +0100

    minor clenaup

--- central/simple_http.py
+++ central/simple_http.py
@@ -2,7 +2,7 @@ import time
 import sqlite3
 from flask import g
 from flask import Flask, request
-from server.database_adapter import DatabaseAdapter
+from central.database_adapter import DatabaseAdapter
 app = Flask(__name__)
 database = DatabaseAdapter()
 
@@ -21,4 +21,5 @@ def new_value():
     return "OK"
 
 
-app.run(port=5050, host="0.0.0.0", debug=True)
+def start():
+    app.run(port=5050, host="0.0.0.0", debug=True)
---
+++ main.py
@@ -0,0 +1,4 @@
+from central.simple_http import start
+
+if __name__=='__main__':
+    start()
